afefda98a1635790dd3a2b71d9c3a25e27e81cf2
gusmt123@gmail.com <gusmt123@gmail.com>
2024-01-04 14:58:57 -0300

    novo classe para controlar o jogo

---
+++ Comeco_jogo.h
@@ -0,0 +1,16 @@
+#include "Personagem.h"
+
+class comeco_jogo
+{
+
+    private:
+    short int hp_goblins_adversarios[5];
+    short int hp_magos_adversarios[3];
+    short int hp_elfos_adversarios[8];
+    public:
+    comeco_jogo(Personagem jogador);
+    short int get_hp_goblins_adversarios[5];
+    short int get_hp_magos_adversarios[3];
+    short int hp_elfos_adversarios[8];
+    
+};
